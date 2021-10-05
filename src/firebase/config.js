3121efafa5f2df205e46da3c074fd9b3e30cffe1
Ashique-Desai <desai.ashique@gmail.com>
2021-10-05 16:33:49 +0530

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyDltCoL5NLYSgeGMf7MNOFk8DfEw6M1-zo",
+  authDomain: "fir-tutorials-c6477.firebaseapp.com",
+  projectId: "fir-tutorials-c6477",
+  storageBucket: "fir-tutorials-c6477.appspot.com",
+  messagingSenderId: "308919463624",
+  appId: "1:308919463624:web:097476357650e7b238fffd",
+  measurementId: "G-SGH2FFFMMQ",
+};
